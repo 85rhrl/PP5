fd60b50ed98fab64f78b412dd9084941d5ed1409
Roberto Rodriguez <85rhrl@gmail.com>
2024-04-09 07:13:07 +0000

    add Wishlist views

--- wishlists/views.py
+++ wishlists/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from PP5.permissions import IsOwnerOrReadOnly
+from wishlists.models import Wishlist
+from wishlists.serializers import WishlistSerializer
 
-# Create your views here.
+
+class WishlistList(generics.ListCreateAPIView):
+    """
+    List wishlists or create a wishlist if logged in.
+    """
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = WishlistSerializer
+    queryset = Wishlist.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class WishlistDetail(generics.RetrieveDestroyAPIView):
+    """
+    Retrieve a wishlist or delete it by id if you own it.
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = WishlistSerializer
+    queryset = Wishlist.objects.all()

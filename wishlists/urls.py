bc2f69d94636d2b49175f193888a924fe5edfeee
Roberto Rodriguez <85rhrl@gmail.com>
2024-04-09 07:15:47 +0000

    add wishlist urls.py

---
+++ wishlists/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from wishlists import views
+
+urlpatterns = [
+    path('wishlists/', views.WishlistList.as_view()),
+    path('wishlists/<int:pk>/', views.WishlistDetail.as_view()),
+]

c062f27eded4a6c5978140855868649ad9512626
Roberto Rodriguez <85rhrl@gmail.com>
2024-04-09 11:28:46 +0000

    add Ownlist view

--- ownlists/views.py
+++ ownlists/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from PP5.permissions import IsOwnerOrReadOnly
+from ownlists.models import Ownlist
+from ownlists.serializers import OwnlistSerializer
 
-# Create your views here.
+
+class OwnlistList(generics.ListCreateAPIView):
+    """
+    List ownlists or create a ownlist if logged in.
+    """
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = OwnlistSerializer
+    queryset = Ownlist.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class OwnlistDetail(generics.RetrieveDestroyAPIView):
+    """
+    Retrieve a ownlist or delete it by id if you own it.
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = OwnlistSerializer
+    queryset = Ownlist.objects.all()

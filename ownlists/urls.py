713553a5be49d80b45a3231606236ab66377037a
Roberto Rodriguez <85rhrl@gmail.com>
2024-04-09 11:30:24 +0000

    add ownlist urls.py

---
+++ ownlists/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from ownlists import views
+
+urlpatterns = [
+    path('ownlists/', views.OwnlistList.as_view()),
+    path('ownlists/<int:pk>/', views.OwnlistDetail.as_view()),
+]
